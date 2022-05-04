4f017079aafd9ade602f9a3cc4b1de8cfc408ec2
FedeRacun <knispel.federico@gmail.com>
2022-05-04 04:15:12 -0300

    se agrega el boton al menu

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  showMenu = false
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onClickMenu(isMenuOpen: any) {
+    let navbar = document.getElementById('menuMobile')
+
+    if (isMenuOpen) {
+      navbar?.classList.add('show-menu')
+      navbar?.classList.remove('hide-menu')
+    } else {
+      navbar?.classList.add('hide-menu')
+      navbar?.classList.remove('show-menu')
+    }
   }
 
 }
