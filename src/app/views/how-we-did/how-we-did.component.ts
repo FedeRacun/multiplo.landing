9bb570339fc56a4b29a7ebe974dab7458a68fa71
FedeRacun <knispel.federico@gmail.com>
2022-05-03 23:34:11 -0300

    se añade video y assets

---
+++ src/app/views/how-we-did/how-we-did.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-how-we-did',
+  templateUrl: './how-we-did.component.html',
+  styleUrls: ['./how-we-did.component.css']
+})
+export class HowWeDidComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
